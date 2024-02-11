8f4cd62f09aa182efcb6abfb9f3a91e1ce03e714
Adam Simpkins <adam@adamsimpkins.net>
2024-02-11 12:28:30 -0800

    Some additional code refactoring between files

--- waymon/src/main.rs
+++ waymon/src/main.rs
@@ -1,8 +1,6 @@
 use anyhow::anyhow;
 use clap::{Parser, ValueHint};
-use gtk::prelude::*;
-use gtk::{glib, Orientation, Window};
-use gtk4_layer_shell::{Edge, Layer, LayerShell};
+use gtk::glib;
 use std::ffi::OsString;
 use std::path::PathBuf;
 
@@ -39,64 +37,9 @@ fn main() -> anyhow::Result<()> {
     // gtk::Application.  I don't really want the gtk Application's handling of application
     // uniqueness or it's command line argument parsing an file open semantics.
     gtk::init()?;
-    init_window(&mut waymon);
+    waymon.create_window();
+    waymon.start();
     let main_loop = glib::MainLoop::new(None, false);
     main_loop.run();
     Ok(())
-}
-
-fn report_css_parsing_error(
-    _css: &gtk::CssProvider,
-    section: &gtk::CssSection,
-    error: &glib::Error,
-) {
-    eprintln!("CSS parsing error at {}: {}\n", section.to_str(), error);
-}
-
-fn on_tick() -> glib::ControlFlow {
-    println!("tick!");
-    glib::ControlFlow::Continue
-}
-
-fn init_window(waymon: &mut waymon::Waymon) {
-    // Create a window and set the title
-    let window = Window::new();
-    window.set_title(Some("waymon"));
-
-    // Configure the window as a layer surface
-    window.init_layer_shell();
-    // Display below normal windows
-    window.set_layer(Layer::Top);
-    // Push other windows out of the way
-    window.auto_exclusive_zone_enable();
-
-    // Anchor to the right edge
-    window.set_anchor(Edge::Right, true);
-    // Anchor to both top and bottom edges, to span the entire height of the
-    // screen.
-    window.set_anchor(Edge::Top, true);
-    window.set_anchor(Edge::Bottom, true);
-
-    window.set_default_size(100, -1);
-
-    let box_widget = gtk::Box::new(Orientation::Vertical, /*spacing*/ 0);
-    box_widget.add_css_class("background");
-    window.set_child(Some(&box_widget));
-
-    let css = gtk::CssProvider::new();
-    css.connect_parsing_error(report_css_parsing_error);
-    css.load_from_path(waymon.css_path());
-    gtk::style_context_add_provider_for_display(
-        &WidgetExt::display(&window),
-        &css,
-        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
-    );
-
-    waymon.add_widgets(&box_widget);
-
-    waymon.start();
-    glib::timeout_add(waymon.interval(), on_tick);
-
-    // Present window
-    window.present();
 }
--- waymon/src/waymon.rs
+++ waymon/src/waymon.rs
@@ -1,14 +1,22 @@
+use crate::config::{Config, CpuWidgetConfig, DiskIOWidgetConfig, WidgetConfig};
 use anyhow::Result;
 use gtk::pango::EllipsizeMode;
 use gtk::prelude::*;
+use gtk::{glib, Orientation, Window};
+use gtk4_layer_shell::{Edge, Layer, LayerShell};
 use std::path::{Path, PathBuf};
 use std::time::Duration;
-use crate::config::{Config, CpuWidgetConfig, DiskIOWidgetConfig, WidgetConfig};
 
 pub struct Waymon {
     config_dir: PathBuf,
     config: Config,
-    timeout_id: Option<gtk::glib::source::SourceId>,
+    timeout_id: Option<glib::source::SourceId>,
+    window: Option<gtk::Window>,
+}
+
+fn on_tick() -> glib::ControlFlow {
+    println!("tick!");
+    glib::ControlFlow::Continue
 }
 
 impl Waymon {
@@ -17,12 +25,14 @@ impl Waymon {
             config_dir: config_dir.to_path_buf(),
             config: Config::load(&config_dir.join("config.toml"))?,
             timeout_id: None,
+            window: None,
         };
         Ok(waymon)
     }
 
     pub fn start(&mut self) {
         assert_eq!(self.timeout_id, None);
+        self.timeout_id = Some(glib::timeout_add(self.interval(), on_tick));
     }
 
     pub fn interval(&self) -> Duration {
@@ -40,6 +50,47 @@ impl Waymon {
         Ok(())
     }
     */
+
+    pub fn create_window(&mut self) {
+        // Create a window and set the title
+        let window = Window::new();
+        window.set_title(Some("waymon"));
+
+        // Configure the window as a layer surface
+        window.init_layer_shell();
+        // Display below normal windows
+        window.set_layer(Layer::Top);
+        // Push other windows out of the way
+        window.auto_exclusive_zone_enable();
+
+        // Anchor to the right edge
+        window.set_anchor(Edge::Right, true);
+        // Anchor to both top and bottom edges, to span the entire height of the
+        // screen.
+        window.set_anchor(Edge::Top, true);
+        window.set_anchor(Edge::Bottom, true);
+
+        window.set_default_size(100, -1);
+
+        let box_widget = gtk::Box::new(Orientation::Vertical, /*spacing*/ 0);
+        box_widget.add_css_class("background");
+        window.set_child(Some(&box_widget));
+
+        let css = gtk::CssProvider::new();
+        css.connect_parsing_error(report_css_parsing_error);
+        css.load_from_path(self.css_path());
+        gtk::style_context_add_provider_for_display(
+            &WidgetExt::display(&window),
+            &css,
+            gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+        );
+
+        self.add_widgets(&box_widget);
+
+        // Present window
+        window.present();
+        self.window = Some(window);
+    }
 
     pub fn add_widgets(&self, container: &gtk::Box) {
         for w in &self.config.widgets {
@@ -70,4 +121,23 @@ impl Waymon {
         label.set_ellipsize(EllipsizeMode::End);
         container.append(&label);
     }
+}
+
+impl Drop for Waymon {
+    fn drop(&mut self) {
+        if let Some(t) = self.timeout_id.take() {
+            t.remove();
+        }
+        if let Some(win) = self.window.take() {
+            win.destroy();
+        }
+    }
+}
+
+fn report_css_parsing_error(
+    _css: &gtk::CssProvider,
+    section: &gtk::CssSection,
+    error: &glib::Error,
+) {
+    eprintln!("CSS parsing error at {}: {}\n", section.to_str(), error);
 }
