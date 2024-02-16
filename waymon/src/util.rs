5dc2719b9692476ab813c4a8194636651c618e9b
Adam Simpkins <adam@adamsimpkins.net>
2024-02-15 20:38:56 -0800

    Improve number precision in chart annotations

--- waymon/src/util.rs
+++ waymon/src/util.rs
@@ -1,15 +1,24 @@
+fn format_f64_helper(value: f64, denom: f64, suffix: &str) -> String {
+    let x = value / denom;
+    if x < 10.0 {
+        format!("{:.2}{}", x, suffix)
+    } else if x < 100.0 {
+        format!("{:.1}{}", x, suffix)
+    } else {
+        format!("{:.0}{}", x, suffix)
+    }
+}
+
 pub fn humanify_f64(value: f64) -> String {
     if value < 1000.0 {
-        return format!("{}B", value as u64);
+        format!("{}B", value as u64)
+    } else if value < 1_000_000.0 {
+        format_f64_helper(value, 1000.0, "KB")
+    } else if value < 1_000_000_000.0 {
+        format_f64_helper(value, 1_000_000.0, "MB")
+    } else if value < 1_000_000_000_000.0 {
+        format_f64_helper(value, 1_000_000_000.0, "GB")
+    } else {
+        format_f64_helper(value, 1_000_000_000_000.0, "TB")
     }
-    if value < 1_000_000.0 {
-        return format!("{}KB", (value as u64) / 1000);
-    }
-    if value < 1_000_000_000.0 {
-        return format!("{}MB", (value as u64) / 1_000_000);
-    }
-    if value < 1_000_000_000_000.0 {
-        return format!("{}GB", (value as u64) / 1_000_000_000);
-    }
-    return format!("{}TB", (value as u64) / 1_000_000_000_000);
 }
