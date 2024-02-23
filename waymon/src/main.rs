d6b95865eacf8fb5045cef6334bf726a1d9542bf
Adam Simpkins <adam@adamsimpkins.net>
2024-02-23 14:13:31 -0800

    Explicitly configure logging with a command line flag

--- waymon/src/main.rs
+++ waymon/src/main.rs
@@ -3,7 +3,10 @@ use clap::{Parser, ValueHint};
 use gtk::glib;
 use std::ffi::OsString;
 use std::path::PathBuf;
-use tracing_subscriber;
+use std::str::FromStr;
+use tracing_subscriber::filter::{LevelFilter, Targets};
+use tracing_subscriber::fmt::Subscriber;
+use tracing_subscriber::prelude::*;
 
 mod bar;
 mod collectors;
@@ -19,6 +22,8 @@ mod widgets;
 struct Opt {
     #[arg(long, value_parser, value_hint=ValueHint::DirPath)]
     config_dir: Option<OsString>,
+    #[arg(short, long)]
+    logging: Option<String>,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -31,7 +36,7 @@ fn main() -> anyhow::Result<()> {
         return Err(anyhow!("unable to determine config directory"));
     };
 
-    tracing_subscriber::fmt::init();
+    init_logging(opts.logging.as_deref())?;
 
     // I'm manually calling gtk::init and driving the glib main loop here, rather than using
     // gtk::Application.  I don't really want the gtk Application's handling of application
@@ -48,5 +53,22 @@ fn main() -> anyhow::Result<()> {
     waymon.start();
     let main_loop = glib::MainLoop::new(None, false);
     main_loop.run();
+    Ok(())
+}
+
+fn init_logging(log_config: Option<&str>) -> anyhow::Result<()> {
+    // Configure logging.
+    // Rather than using the RUST_LOG environment variable, this uses an explicit --logging command
+    // line argument, and explicitly fails if there is a parse error in the settings.
+    let log_filter = if let Some(config_str) = log_config {
+        Targets::from_str(&config_str)?
+    } else {
+        // Show warning and above by default
+        Targets::new().with_default(LevelFilter::WARN)
+    };
+    let log_subscriber = Subscriber::builder()
+        .with_max_level(LevelFilter::TRACE)
+        .finish();
+    log_subscriber.with(log_filter).try_init()?;
     Ok(())
 }
